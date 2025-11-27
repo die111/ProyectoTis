c2c331bc9e6cf3eb997ead29469fd6ed8d842c02
Diego Villarroel <die13501@gmail.com>
2025-11-27 19:03:50 -0400

    chore: integracion de  Tailwind CSS con Vite y configuracion de PostCSS

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,7 +20,6 @@ class DatabaseSeeder extends Seeder
             LevelSeeder::class,
             PermissionSeeder::class,
             CategoriaSeeder::class,
-            \Database\Seeders\AuditSeeder::class,
         ]);
     }
 }
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,7 +15,6 @@
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
     <!-- Styles -->
-    <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 
     <!-- Scripts -->
