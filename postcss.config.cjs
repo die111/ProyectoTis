b784585d0e595764224429bfd163c611f22b1f82
Diego Edwin Villarroel Saavedra <die13501@gmail.com>
2025-11-27 20:38:14 -0400

    refactor: actualizacion build scripts y PostCSS cconfiguracion de tailwind 3.3.3

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,10 +1,4 @@
 module.exports = {
-  plugins: [
-    require('@tailwindcss/postcss')(),
-    require('autoprefixer')(),
-  ],
-}
-module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,6 @@
-export default async () => {
-  const tailwindPostcss = (await import('@tailwindcss/postcss')).default;
-  const autoprefixer = (await import('autoprefixer')).default;
-  return {
-    plugins: [
-      tailwindPostcss(),
-      autoprefixer(),
-    ],
-  };
+export default {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  }
 };
